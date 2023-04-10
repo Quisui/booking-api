240cde187aed698ce4e3c3c87b804f93519e75a8
Carlos Enrique Alvarado Legrand <carlos@tagleaf.com>
2023-04-10 11:47:22 -0600

    Adding initial tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('auth/register', App\Http\Controllers\Auth\RegisterController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    // No owner/user grouping, for now, will do it later with more routes
+    Route::get(
+        'owner/properties',
+        [\App\Http\Controllers\Owner\PropertyController::class, 'index']
+    );
+    Route::get(
+        'user/bookings',
+        [\App\Http\Controllers\User\BookingController::class, 'index']
+    );
+});
