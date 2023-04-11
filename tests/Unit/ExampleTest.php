7d816d5b2b0c6a71bd5e51341fdc7e48dfafd614
Carlos Enrique Alvarado Legrand <carlos@tagleaf.com>
2023-04-11 12:43:45 -0600

    missing unit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
